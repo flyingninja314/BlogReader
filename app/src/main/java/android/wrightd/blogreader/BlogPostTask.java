9e28d39bf663ab16638dde928f43f609502835c5
flyingninja314 <drw.ninja@gmail.com>
2014-10-08 13:45:10 -0700

    Milestone 11
    ayyyyyyyyyy using the logcat thingamajig

--- app/src/main/java/android/wrightd/blogreader/BlogPostTask.java
+++ app/src/main/java/android/wrightd/blogreader/BlogPostTask.java
@@ -22,7 +22,7 @@ public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
             int responseCode = connection.getResponseCode();
 
             if(responseCode == HttpURLConnection.HTTP_OK){
-
+//                Log.i("BlogPostTask", "Successful Connection " + responseCode);
             }
         }
         catch(MalformedURLException error){
