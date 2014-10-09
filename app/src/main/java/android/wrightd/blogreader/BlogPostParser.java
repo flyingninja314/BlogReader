54c00fb4ecbfaa6b7fd2ff6e295c3a5d728a5a98
flyingninja314 <drw.ninja@gmail.com>
2014-10-09 13:38:16 -0700

    Milestone 15
    reading lines from the input string
    try catch and such

--- app/src/main/java/android/wrightd/blogreader/BlogPostParser.java
+++ app/src/main/java/android/wrightd/blogreader/BlogPostParser.java
@@ -1,8 +1,11 @@
 package android.wrightd.blogreader;
 
+import android.util.Log;
+
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -24,6 +27,18 @@ public class BlogPostParser {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         StringBuilder builder = new StringBuilder();
         JSONObject jsonObject = null;
+
+        String line;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+
+            }
+        }
+        catch(IOException error) {
+            Log.e("BlogPostParser", "IOException: " + error);
+        }
+
         return jsonObject;
     }
 }
