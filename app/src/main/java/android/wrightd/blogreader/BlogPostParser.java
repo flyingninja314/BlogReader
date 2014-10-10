421f83d7ed977c6e4a7e34c327e985dd09c01c55
flyingninja314 <drw.ninja@gmail.com>
2014-10-10 14:01:37 -0700

    Milestone 19
    making a function to READ the frunking JSONObject

--- app/src/main/java/android/wrightd/blogreader/BlogPostParser.java
+++ app/src/main/java/android/wrightd/blogreader/BlogPostParser.java
@@ -10,9 +10,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class BlogPostParser {
     private static BlogPostParser parser;
+    public ArrayList<BlogPost> posts;
 
     private BlogPostParser() {
 
@@ -47,5 +49,9 @@ public class BlogPostParser {
         }
 
         return jsonObject;
+    }
+
+    public void readFeed(JSONObject jsonObject) {
+        
     }
 }
