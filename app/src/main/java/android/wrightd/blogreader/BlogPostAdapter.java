e1ef3718ea60acc84501fd8f4a19021b4d9d9611
flyingninja314 <drw.ninja@gmail.com>
2014-10-16 13:56:37 -0700

    Milestone 29
    we gonna override getView

--- app/src/main/java/android/wrightd/blogreader/BlogPostAdapter.java
+++ app/src/main/java/android/wrightd/blogreader/BlogPostAdapter.java
@@ -1,6 +1,8 @@
 package android.wrightd.blogreader;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
@@ -9,5 +11,10 @@ public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
 
     public BlogPostAdapter(Context context, ArrayList<BlogPost> posts) {
         super(context, 0, posts);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        
     }
 }
