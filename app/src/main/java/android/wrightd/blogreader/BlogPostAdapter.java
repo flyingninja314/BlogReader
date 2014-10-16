140b906dea373938a63d73613a9d863dac8af8c5
flyingninja314 <drw.ninja@gmail.com>
2014-10-16 13:59:20 -0700

    Milestone 30
    Part one of the HARD way

--- app/src/main/java/android/wrightd/blogreader/BlogPostAdapter.java
+++ app/src/main/java/android/wrightd/blogreader/BlogPostAdapter.java
@@ -15,6 +15,6 @@ public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        
+        BlogPost post = getItem(position);
     }
 }
