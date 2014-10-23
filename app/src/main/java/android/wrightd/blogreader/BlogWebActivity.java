ef955f460935b1d823052d915ecc3151d9c19654
flyingninja314 <drw.ninja@gmail.com>
2014-10-23 13:36:21 -0700

    Milestone 36
    creating BlogWebActivity class
    moises said to write R.layout.activity_blog_web  but my computer said that doesnt exisst, so I wrote blog_web_activity

---
+++ app/src/main/java/android/wrightd/blogreader/BlogWebActivity.java
@@ -0,0 +1,12 @@
+package android.wrightd.blogreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlogWebActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blog_web_activity);
+    }
+}
