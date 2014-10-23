f6dfba502c8fdf04bf570441b5a5f4f685ae9ecc
flyingninja314 <drw.ninja@gmail.com>
2014-10-23 14:00:04 -0700

    Milestone 38
    retrieving intent

--- app/src/main/java/android/wrightd/blogreader/BlogWebActivity.java
+++ app/src/main/java/android/wrightd/blogreader/BlogWebActivity.java
@@ -1,12 +1,21 @@
 package android.wrightd.blogreader;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class BlogWebActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.blog_web_activity);
+
+        Intent intent = getIntent();
+        Uri blogUri = intent.getData();
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.loadUrl(blogUri.toString());
     }
 }
