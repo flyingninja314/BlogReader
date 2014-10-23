417c6e833d76e870bdbda23662e70ee6098d9991
flyingninja314 <drw.ninja@gmail.com>
2014-10-23 13:47:01 -0700

    Milestone 37
    INTENT

--- app/src/main/java/android/wrightd/blogreader/BlogActivity.java
+++ app/src/main/java/android/wrightd/blogreader/BlogActivity.java
@@ -1,6 +1,8 @@
 package android.wrightd.blogreader;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -38,6 +40,12 @@ public class BlogActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.i("BlogActivity", "Title: " + BlogPostParser.get().posts.get(position).title);
+
+                Intent intent = new Intent(getApplicationContext(), BlogWebActivity.class);
+                Uri blogUri = Uri.parse(BlogPostParser.get().posts.get(position).url);
+                intent.setData(blogUri);
+
+                startActivity(intent);
             }
         });
 
