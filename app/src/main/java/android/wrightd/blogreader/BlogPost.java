f6396f52700c727f99476ef91ede661d9682774f
flyingninja314 <drw.ninja@gmail.com>
2014-10-28 13:31:26 -0700

    Intermediate Milestone 3
    storing all the information for the blog post

--- app/src/main/java/android/wrightd/blogreader/BlogPost.java
+++ app/src/main/java/android/wrightd/blogreader/BlogPost.java
@@ -5,10 +5,16 @@ package android.wrightd.blogreader;
 public class BlogPost {
     public final String title;
     public final String url;
+    public final String date;
+    public final String author;
+    public final String thumbnail;
 
-    public BlogPost(String title, String url){
+    public BlogPost(String title, String url, String date, String author, String thumbnail){
         this.title = title;
         this.url = url;
+        this.date = date;
+        this.author = author;
+        this.thumbnail = thumbnail;
     }
 
 //    @Override
